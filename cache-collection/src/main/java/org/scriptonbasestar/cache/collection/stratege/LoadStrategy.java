0fb4045bd8a68026e392db97452d6dad46c3c849
cee <archmagece@gmail.com>
2016-11-14 17:41:21 +0900

    캐싱전략 변경

---
+++ cache-collection/src/main/java/org/scriptonbasestar/cache/collection/stratege/LoadStrategy.java
@@ -0,0 +1,11 @@
+package org.scriptonbasestar.cache.collection.stratege;
+
+/**
+ * @author archmagece
+ * @with bs-tools-java
+ * @since 2016-11-14
+ */
+public enum LoadStrategy {
+	SYNC,
+	ASYNC
+}
---
+++ cache-collection/src/main/java/org/scriptonbasestar/cache/collection/stratege/NoticeStrategy.java
@@ -0,0 +1,10 @@
+package org.scriptonbasestar.cache.collection.stratege;
+
+/**
+ * @author archmagece
+ * @since 2016-11-14
+ */
+public enum NoticeStrategy {
+	PUSH,
+	LISTEN
+}
