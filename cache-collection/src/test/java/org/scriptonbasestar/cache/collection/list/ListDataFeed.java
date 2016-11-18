3995792b94858e9aee94ad091ed6f62b4f451469
cee <archmagece@gmail.com>
2016-11-18 18:42:08 +0900

    ㅇ

--- cache-collection/src/test/java/org/scriptonbasestar/cache/collection/list/ListDataFeed.java
+++ cache-collection/src/test/java/org/scriptonbasestar/cache/collection/list/ListDataFeed.java
@@ -1,5 +1,7 @@
 package org.scriptonbasestar.cache.collection.list;
 
+import org.scriptonbasestar.cache.core.exception.SBCacheLoadFailException;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,27 +9,32 @@ import java.util.List;
  * @author archmagece
  * @since 2016-11-07
  */
-public class ListDataFeed {
+public class ListDataFeed implements SBCacheListDAO<String> {
 	private static final List<String> dataList = new ArrayList<>();
+
 	static {
-		for(int i=0;i<30;i++){
-			dataList.add(""+i+i+i+i+i);
+		for (int i = 0; i < 30; i++) {
+			dataList.add("" + i + i + i + i + i);
 		}
 	}
-	public static List<String> getDataList(){
-		try {
-			Thread.sleep(3000);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-		return dataList;
-	}
-	public static String getData(int index){
+
+	@Override
+	public String load(int index) throws SBCacheLoadFailException {
 		try {
 			Thread.sleep(3000);
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
 		return dataList.get(index);
+	}
+
+	@Override
+	public List loadAll() throws SBCacheLoadFailException {
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		return dataList;
 	}
 }
