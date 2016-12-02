8815e15cd593f016e99c1e1b32a7335c1465746a
cee <archmagece@gmail.com>
2016-12-02 12:03:20 +0900

    pom정리

--- cache-collection/src/test/java/org/scriptonbasestar/cache/collection/list/ListDataFeed.java
+++ cache-collection/src/test/java/org/scriptonbasestar/cache/collection/list/ListDataFeed.java
@@ -1,6 +1,5 @@
 package org.scriptonbasestar.cache.collection.list;
 
-import org.scriptonbasestar.cache.core.exception.SBCacheLoadFailException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -9,32 +8,32 @@ import java.util.List;
  * @author archmagece
  * @since 2016-11-07
  */
-public class ListDataFeed implements SBCacheListDAO<String> {
-	private static final List<String> dataList = new ArrayList<>();
-
-	static {
-		for (int i = 0; i < 30; i++) {
-			dataList.add("" + i + i + i + i + i);
-		}
-	}
-
-	@Override
-	public String load(int index) throws SBCacheLoadFailException {
-		try {
-			Thread.sleep(3000);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-		return dataList.get(index);
-	}
-
-	@Override
-	public List loadAll() throws SBCacheLoadFailException {
-		try {
-			Thread.sleep(3000);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-		return dataList;
-	}
-}
+//public class ListDataFeed implements SBCacheListDAO<String> {
+//	private static final List<String> dataList = new ArrayList<>();
+//
+//	static {
+//		for (int i = 0; i < 30; i++) {
+//			dataList.add("" + i + i + i + i + i);
+//		}
+//	}
+//
+//	@Override
+//	public String load(int index) throws SBCacheLoadFailException {
+//		try {
+//			Thread.sleep(3000);
+//		} catch (InterruptedException e) {
+//			e.printStackTrace();
+//		}
+//		return dataList.get(index);
+//	}
+//
+//	@Override
+//	public List loadAll() throws SBCacheLoadFailException {
+//		try {
+//			Thread.sleep(3000);
+//		} catch (InterruptedException e) {
+//			e.printStackTrace();
+//		}
+//		return dataList;
+//	}
+//}
