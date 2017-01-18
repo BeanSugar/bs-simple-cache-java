a0ede5e408991f47302d613cda26062ed5b24c0f
cee <archmagece@gmail.com>
2017-01-18 16:07:57 +0900

    쓰레드 오류가능성 체크

--- cache-collection/src/test/java/org/beansugar/cache/collection/map/BSCacheMapTest.java
+++ cache-collection/src/test/java/org/beansugar/cache/collection/map/BSCacheMapTest.java
@@ -1,47 +1,161 @@
 package org.beansugar.cache.collection.map;
 
+import lombok.extern.slf4j.Slf4j;
 import org.beansugar.cache.core.exception.BSCacheLoadFailException;
+import org.joda.time.DateTime;
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.Map;
+import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author archmagece
  * @since 2016-11-07
  */
+@Slf4j
 public class BSCacheMapTest {
 
-	@Test
-	public void testLoad(){
-		final BSCacheMapFeedingLoader dataFeed = new BSCacheMapFeedingLoader();
+	private final BSCacheMapDataFeeder dataFeeder = new BSCacheMapDataFeeder();
+	private BSCacheMap<Long, String> cacheData = new BSCacheMap<>(new BSCacheMapLoader<Long, String>() {
+		@Override
+		public String loadOne(Long key) throws BSCacheLoadFailException {
+			return dataFeeder.loadOne(key);
+		}
 
-		System.out.println(dataFeed.loadOne(0L));
-//		System.out.println(dataFeed.loadAll());
-		System.out.println(dataFeed.loadOne(0L));
-//		System.out.println(dataFeed.loadAll());
-		System.out.println(dataFeed.loadOne(0L));
-//		System.out.println(dataFeed.loadAll());
+		@Override
+		public Map<Long, String> loadAll() throws BSCacheLoadFailException {
+			return dataFeeder.loadAll();
+		}
+	}, 10);
+
+	//로딩 딜레이 확인
+	@Test
+	public void subLoadOne로딩딜레이확인() {
+		int 반복횟수 = 10;
+
+		DateTime start1 = DateTime.now();
+		for (int i = 0; i < 반복횟수; i++) {
+			System.out.println(dataFeeder.loadOne(0L));
+		}
+		DateTime end1 = DateTime.now();
+		long result1 = end1.getMillis() - start1.getMillis();
+		System.out.println(result1);
+
+		DateTime start2 = DateTime.now();
+		for (int i = 0; i < 반복횟수; i++) {
+			System.out.println(cacheData.get(0L));
+		}
+		DateTime end2 = DateTime.now();
+		long result2 = end2.getMillis() - start2.getMillis();
+		System.out.println(result2);
+
+		System.out.println("결과시간 비교(ms) result1 : " + result1);
+		System.out.println("결과시간 비교(ms) result2 : " + result2);
+		System.out.println("! 결과시간이 resutl1 > result2인게 정상");
+		Assert.assertTrue(result1 > result2);
 	}
 
 	@Test
-	public void testLoadCache(){
-		final BSCacheMapFeedingLoader dataFeed = new BSCacheMapFeedingLoader();
-		BSCacheMap<Long, String> cacheData = new BSCacheMap<>(new BSCacheMapLoader<Long, String>(){
-			@Override
-			public String loadOne(Long key) throws BSCacheLoadFailException {
-				return dataFeed.loadOne(key);
-			}
-			@Override
-			public Map<Long, String> loadAll() throws BSCacheLoadFailException {
-				return dataFeed.loadAll();
-			}
-		}, 10);
+	public void subLoadAll로딩딜레이확인() {
+		int 반복횟수 = 5;
 
-		System.out.println(cacheData.get(0L));
-		System.out.println(cacheData);
-		System.out.println(cacheData.get(0L));
-		System.out.println(cacheData);
-		System.out.println(cacheData.get(0L));
-		System.out.println(cacheData);
+		DateTime start1 = DateTime.now();
+		for (int i = 0; i < 반복횟수; i++) {
+			System.out.println(dataFeeder.loadAll());
+		}
+		DateTime end1 = DateTime.now();
+		long result1 = end1.getMillis() - start1.getMillis();
+		System.out.println(result1);
+
+		DateTime start2 = DateTime.now();
+		for (int i = 0; i < 반복횟수; i++) {
+			System.out.println(cacheData.toString());
+		}
+		DateTime end2 = DateTime.now();
+		long result2 = end2.getMillis() - start2.getMillis();
+		System.out.println(result2);
+
+		System.out.println("결과시간 비교(ms) result1 : " + result1);
+		System.out.println("결과시간 비교(ms) result2 : " + result2);
+		System.out.println("! 결과시간이 resutl1 > result2인게 정상");
+		Assert.assertTrue(result1 > result2);
 	}
+
+	@Test
+	public void test타임아웃이후_가져오기() {
+		long now;
+		long timeSpent;
+
+		now = DateTime.now().getMillis();
+		System.out.println("first move");
+		for(int i=0;i<5;i++){
+			cacheData.get((long) i);
+		}
+		timeSpent = DateTime.now().getMillis() - now;
+		System.out.println("시간소요 first move : " + timeSpent);
+
+		now = DateTime.now().getMillis();
+		System.out.println("second move");
+		for(long key : cacheData.keySet()){
+			cacheData.get(key);
+		}
+		timeSpent = DateTime.now().getMillis() - now;
+		System.out.println("시간소요 second move : " + timeSpent);
+
+		now = DateTime.now().getMillis();
+		System.out.println("third move");
+		for(long key : cacheData.keySet()){
+			cacheData.get(key);
+		}
+		timeSpent = DateTime.now().getMillis() - now;
+		System.out.println("시간소요 third move : " + timeSpent);
+
+		System.out.println("========================");
+		System.out.println("쉬는시간 10초");
+		try {
+			Thread.sleep(1000*20);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		now = DateTime.now().getMillis();
+		System.out.println("last move");
+		for(long key : cacheData.keySet()){
+			cacheData.get(key);
+		}
+		timeSpent = DateTime.now().getMillis() - now;
+		System.out.println("시간소요 last move : " + timeSpent);
+
+	}
+
+	private ExecutorService executor;
+	@Test
+	public void test멀티쓰레드환경에서싸이코_IN올OUT() throws InterruptedException {
+		int maxThreadCount = 100;
+		executor = Executors.newFixedThreadPool(maxThreadCount);
+
+		Runnable runnable = new Runnable() {
+			Random random = new Random();
+			@Override
+			public void run() {
+//				long key = Math.abs(random.nextLong() % 30);
+				long key = 3;
+				String value0 = cacheData.get(key);
+				cacheData.put(key, value0+"0");
+				String value1 = cacheData.get(key);
+				System.out.println(key + "  " + value0 + "  " + value1 + "  " + value0.equals(value1));
+				Assert.assertEquals(value0, value1);
+				Assert.assertTrue(value1.equals(value0+"0"));
+			}
+		};
+		for(int i=0;i<maxThreadCount;i++){
+			executor.submit(runnable);
+		}
+		executor.awaitTermination(10, TimeUnit.SECONDS);
+	}
+
 }
